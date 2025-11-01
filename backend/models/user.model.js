e875f7be01e959206c14a79d3f091b057906bed8
nirvair09 <chandradeep262003@gmail.com>
2025-11-01 11:17:01 +0530

    user model added

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    lastLogin:{
+        type:Date,
+        default:Date.now,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    resetPasswordToken:String,
+    resetPasswordTokenExpiresIn:Date,
+    verificationToken:String,
+    verificationTokenExpiresIn:Date,
+    
+},{
+    timestamps:true
+});
+
+const User=mongoose.model("User",userSchema);
+
+export default User;
