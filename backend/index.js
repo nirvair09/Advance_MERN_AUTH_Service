0471ab40198b9d010344c3e09f01e38c1257ff0d
nirvair09 <chandradeep262003@gmail.com>
2025-11-01 11:24:18 +0530

    cors implemented and auth-routes added

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-//database connection
-connectDB();
+
+
 
 const app= express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+
+app.use("/api/v1/auth",authRoutes);
+
+
+
 const PORT=process.env.PORT || 5000;
 app.get("/",(req,res)=>{
     console.log("Hello from backend of Advance Auth system");
 })
 
-app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT,()=>{ 
+    //database connection
+    connectDB();
+    console.log(`Server running on port ${PORT}`)
+
+});
